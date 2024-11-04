bbd49e938b94ec6ba3fa1e54204018e5ccfa1668
emilie-h <emilie.bressoud@heig-vd.ch>
2024-11-04 08:18:55 +0100

    fix: fix package name in test

--- app/src/androidTest/java/ch/heigvd/iict/daa/labo3/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/ch/heigvd/iict/daa/labo3/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("ch.heigvd.iict.daa.daabasictemplate", appContext.packageName)
+        assertEquals("ch.heigvd.iict.daa.labo3", appContext.packageName)
     }
 }
